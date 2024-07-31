6df60d9bde730bc73d7a5210a8a7771d61038c57
theinfin1ty <anantgaurav1010@gmail.com>
2024-07-31 19:12:18 +0530

    Added tests for CRUD operations

---
+++ db/sqlc/main_test.go
@@ -0,0 +1,29 @@
+package db
+
+import (
+	"database/sql"
+	"log"
+	"os"
+	"testing"
+
+	_ "github.com/lib/pq"
+)
+
+const (
+	dbDriver = "postgres"
+	dbSource = "postgresql://root:password@localhost:5432/go_bank?sslmode=disable"
+)
+
+var testQueries *Queries
+
+func TestMain(m *testing.M) {
+	conn, err := sql.Open(dbDriver, dbSource)
+
+	if err != nil {
+		log.Fatal("Error connecting to database:", err)
+	}
+
+	testQueries = New(conn)
+
+	os.Exit(m.Run())
+}
