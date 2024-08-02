1ebddbe88f7f614dfb824837e37afb9875fe100d
theinfin1ty <anantgaurav1010@gmail.com>
2024-08-02 23:49:43 +0530

    Updated transaction to avoid deadlocks and added tests for the same

--- db/sqlc/entry_test.go
+++ db/sqlc/entry_test.go
@@ -2,7 +2,6 @@ package db
 
 import (
 	"context"
-	"database/sql"
 	"testing"
 	"time"
 
@@ -10,14 +9,13 @@ import (
 	"github.com/theinfin1ty/go-bank/util"
 )
 
-func createRandomEntry(t *testing.T) Entry {
+func createRandomEntry(t *testing.T, account Account) Entry {
 	arg := CreateEntryParams{
-		AccountID: util.RandomInt(1, 5),
+		AccountID: account.ID,
 		Amount:    util.RandomMoney(),
 	}
 
 	entry, err := testQueries.CreateEntry(context.Background(), arg)
-
 	require.NoError(t, err)
 	require.NotEmpty(t, entry)
 
@@ -30,14 +28,10 @@ func createRandomEntry(t *testing.T) Entry {
 	return entry
 }
 
-func TestCreateEntry(t *testing.T) {
-	createRandomEntry(t)
-}
-
 func TestGetEntry(t *testing.T) {
-	entry1 := createRandomEntry(t)
+	account := createRandomAccount(t)
+	entry1 := createRandomEntry(t, account)
 	entry2, err := testQueries.GetEntry(context.Background(), entry1.ID)
-
 	require.NoError(t, err)
 	require.NotEmpty(t, entry2)
 
@@ -47,41 +41,10 @@ func TestGetEntry(t *testing.T) {
 	require.WithinDuration(t, entry1.CreatedAt, entry2.CreatedAt, time.Second)
 }
 
-func TestUpdateEntry(t *testing.T) {
-	entry1 := createRandomEntry(t)
-
-	arg := UpdateEntryParams{
-		ID:     entry1.ID,
-		Amount: util.RandomMoney(),
-	}
-
-	entry2, err := testQueries.UpdateEntry(context.Background(), arg)
-
-	require.NoError(t, err)
-	require.NotEmpty(t, entry2)
-
-	require.Equal(t, entry1.ID, entry2.ID)
-	require.Equal(t, arg.Amount, entry2.Amount)
-	require.WithinDuration(t, entry1.CreatedAt, entry2.CreatedAt, time.Second)
-}
-
-func TestDeleteEntry(t *testing.T) {
-	entry1 := createRandomEntry(t)
-
-	err := testQueries.DeleteEntry(context.Background(), entry1.ID)
-
-	require.NoError(t, err)
-
-	entry2, err := testQueries.GetEntry(context.Background(), entry1.ID)
-
-	require.Error(t, err)
-	require.EqualError(t, err, sql.ErrNoRows.Error())
-	require.Empty(t, entry2)
-}
-
 func TestListEntries(t *testing.T) {
+	account := createRandomAccount(t)
 	for i := 0; i < 10; i++ {
-		createRandomEntry(t)
+		createRandomEntry(t, account)
 	}
 
 	arg := ListEntriesParams{
@@ -90,7 +53,6 @@ func TestListEntries(t *testing.T) {
 	}
 
 	entries, err := testQueries.ListEntries(context.Background(), arg)
-
 	require.NoError(t, err)
 	require.Len(t, entries, 5)
 
--- db/sqlc/transfer_test.go
+++ db/sqlc/transfer_test.go
@@ -2,7 +2,6 @@ package db
 
 import (
 	"context"
-	"database/sql"
 	"testing"
 	"time"
 
@@ -10,15 +9,14 @@ import (
 	"github.com/theinfin1ty/go-bank/util"
 )
 
-func createRandomTransfer(t *testing.T) Transfer {
+func createRandomTransfer(t *testing.T, account1, account2 Account) Transfer {
 	arg := CreateTransferParams{
-		FromAccountID: util.RandomInt(1, 5),
-		ToAccountID:   util.RandomInt(1, 5),
+		FromAccountID: account1.ID,
+		ToAccountID:   account2.ID,
 		Amount:        util.RandomMoney(),
 	}
 
 	transfer, err := testQueries.CreateTransfer(context.Background(), arg)
-
 	require.NoError(t, err)
 	require.NotEmpty(t, transfer)
 
@@ -33,13 +31,17 @@ func createRandomTransfer(t *testing.T) Transfer {
 }
 
 func TestCreateTransfer(t *testing.T) {
-	createRandomTransfer(t)
+	account1 := createRandomAccount(t)
+	account2 := createRandomAccount(t)
+	createRandomTransfer(t, account1, account2)
 }
 
 func TestGetTransfer(t *testing.T) {
-	transfer1 := createRandomTransfer(t)
-	transfer2, err := testQueries.GetTransfer(context.Background(), transfer1.ID)
+	account1 := createRandomAccount(t)
+	account2 := createRandomAccount(t)
+	transfer1 := createRandomTransfer(t, account1, account2)
 
+	transfer2, err := testQueries.GetTransfer(context.Background(), transfer1.ID)
 	require.NoError(t, err)
 	require.NotEmpty(t, transfer2)
 
@@ -50,41 +52,13 @@ func TestGetTransfer(t *testing.T) {
 	require.WithinDuration(t, transfer1.CreatedAt, transfer2.CreatedAt, time.Second)
 }
 
-func TestUpdateTransfer(t *testing.T) {
-	transfer1 := createRandomTransfer(t)
+func TestListTransfer(t *testing.T) {
+	account1 := createRandomAccount(t)
+	account2 := createRandomAccount(t)
 
-	arg := UpdateTransferParams{
-		ID:     transfer1.ID,
-		Amount: util.RandomMoney(),
-	}
-
-	transfer2, err := testQueries.UpdateTransfer(context.Background(), arg)
-
-	require.NoError(t, err)
-	require.NotEmpty(t, transfer2)
-
-	require.Equal(t, transfer1.ID, transfer2.ID)
-	require.Equal(t, arg.Amount, transfer2.Amount)
-	require.WithinDuration(t, transfer1.CreatedAt, transfer2.CreatedAt, time.Second)
-}
-
-func TestDeletetransfer(t *testing.T) {
-	transfer1 := createRandomTransfer(t)
-
-	err := testQueries.DeleteTransfer(context.Background(), transfer1.ID)
-
-	require.NoError(t, err)
-
-	transfer2, err := testQueries.GetTransfer(context.Background(), transfer1.ID)
-
-	require.Error(t, err)
-	require.EqualError(t, err, sql.ErrNoRows.Error())
-	require.Empty(t, transfer2)
-}
-
-func TestListTransfers(t *testing.T) {
-	for i := 0; i < 10; i++ {
-		createRandomTransfer(t)
+	for i := 0; i < 5; i++ {
+		createRandomTransfer(t, account1, account2)
+		createRandomTransfer(t, account2, account1)
 	}
 
 	arg := ListTransfersParams{
@@ -93,11 +67,11 @@ func TestListTransfers(t *testing.T) {
 	}
 
 	transfers, err := testQueries.ListTransfers(context.Background(), arg)
-
 	require.NoError(t, err)
 	require.Len(t, transfers, 5)
 
 	for _, transfer := range transfers {
 		require.NotEmpty(t, transfer)
+		// require.True(t, transfer.FromAccountID == account1.ID || transfer.ToAccountID == account1.ID)
 	}
 }
